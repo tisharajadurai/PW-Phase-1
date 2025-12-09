3d5c0c53b9db222501b2c5d47cc7c38dd9573e24
tisharajadurai <160521991+tisharajadurai@users.noreply.github.com>
2025-12-09 18:17:40 +0530

    Refactor main.py for better organization and clarity
    Refactor main.py to improve structure and functionality.

--- main.py
+++ main.py
@@ -376,4 +376,6 @@ def get_history():
 
 if __name__ == '__main__':
     print("Backend running at http://127.0.0.1:5000")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+
+    
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
